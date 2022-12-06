1cb95fbf222712c5d6a8c8eb702dabe8a1df747c
masachika-kamada <masachika.kmd@gmail.com>
2022-12-06 14:00:18 +0900

    workspaceのマウント

--- ros_noetic/run.sh
+++ ros_noetic/run.sh
@@ -1,10 +1,15 @@
 xhost +local:
 
+if [ ! -d "workspace" ]; then
+    mkdir workspace
+fi
+
 docker run -it \
     -e DISPLAY=unix${DISPLAY} \
     -e QT_X11_NO_MITSHM=1 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $HOME/.Xauthority:/root/.Xauthority \
+    -v $PWD/workspace:/root/catkin_ws \
     --gpus all \
     --name=ros_noetic \
     ros_noetic \
