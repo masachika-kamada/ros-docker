beb9664596e1236c6b852bfd49529ade37cf0e39
masachika-kamada <masachika.kmd@gmail.com>
2022-12-09 13:24:55 +0900

    ros-melodic用のdockerを追加

---
+++ ros_melodic/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+xhost +local:
+
+if [ ! -d "workspace" ]; then
+    mkdir workspace
+fi
+
+docker run -it \
+    -e DISPLAY=unix${DISPLAY} \
+    -e QT_X11_NO_MITSHM=1 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $HOME/.Xauthority:/home/ubuntu/.Xauthority \
+    -v $PWD/workspace:/home/ubuntu/catkin_ws \
+    --gpus all \
+    --name=ros-melodic \
+    ros-melodic \
+    bash
